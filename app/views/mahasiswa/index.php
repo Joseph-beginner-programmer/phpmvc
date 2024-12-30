4089e4f4f49f26416294053daf2a4913d662c648
joseph-beginner-programmer <joseptan1512@outlook.com>
2024-12-30 14:01:25 +0700

    fitur flash message

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,12 +1,19 @@
 <div class="container mt-3">
+
     <div class="row">
-        <div class="col-6">
+      <div class="col-lg-6">
+        <?php Flasher::flash(); ?>
+      </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
-            Launch demo modal
+            Tambah Data Mahasiswa
         </button>
         <br></br>
 
-            <h3>Mahasiswa</h3>
+            <h3>Daftar Mahasiswa</h3>
             
                 <ul class="list-group">
                     <?php foreach($data['mhs'] as $mhs) : ?>
